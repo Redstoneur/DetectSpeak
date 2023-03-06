4aae5243e106e97ac65128b21bc473122ef2a957
Redstoneur <alfa.aog36@gmail.com>
2023-03-06 21:13:13 +0100

    Version : v0.2.0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 import tkinter as tk
 import json
 import threading
+import os
 
 
 # PyAudio
@@ -16,7 +17,10 @@ class MessageBox(tk.Tk):
 
 
 class MainWindow(tk.Tk):
-    def __init__(self):
+    Name_of_the_file_to_save_the_counter: str = "counter.json"
+    Name_of_the_file_to_read_the_words_to_detect: str = "mots.json"
+
+    def __init__(self) -> None:
         super().__init__()
         self.title("Détection de parole en temps réel")
 
@@ -41,79 +45,125 @@ class MainWindow(tk.Tk):
         stop_button = tk.Button(self, text="Arrêter l'enregistrement", command=self.stop_recording, state=tk.DISABLED)
         stop_button.pack(padx=10, pady=10)
 
+        # Ajouter l'étiquette d'information pour afficher les messages d'erreur
+        self.info_label = tk.Label(self, text="")
+        self.info_label.pack(padx=10, pady=10)
+
         # Ajouter l'étiquette de sortie pour afficher le nombre d'occurrences
         self.output_label = tk.Label(self, text="")
         self.output_label.pack(padx=10, pady=10)
 
-        # Charger les mots à détecter à partir du fichier JSON
-        with open("mots.json", "r") as f:
-            self.mots = json.load(f)
+        self.counter: dict = {}
 
-        self.recording_thread = None
+        # Charger les mots à détecter à partir du fichier JSON
+        try:
+            with open(self.Name_of_the_file_to_read_the_words_to_detect, "r") as f:
+                self.mots: list = json.load(f)
+        except FileNotFoundError:
+            MessageBox("Le fichier mots.json n'a pas été trouvé").mainloop()
+
+        for mot in self.mots:
+            self.counter[mot] = 0
+
+        self.stop: bool = True
+        self.thread: threading.Thread = None
+        self.thread_C: threading.Thread = None
+        self.thread_C_All_Words: threading.Thread = None
 
     def show(self):
         self.mainloop()
 
-    def start_recording(self):
-        # Désactiver le bouton de démarrage et activer le bouton d'arrêt
-        self.textbox.delete("1.0", tk.END)
-        self.output_label.config(text="")
-        self.recording_thread = threading.Thread(target=self.detect)
-        self.recording_thread.start()
-        self.after(100, self.update_progressbar)
-        self.progressbar_start_time = self.r.get_current_time()
-        self.start_button.config(state=tk.DISABLED)
-        self.stop_button.config(state=tk.NORMAL)
+    def start_recording(self) -> None:
+        self.stop: bool = False
 
-    def stop_recording(self):
-        # Arrêter le thread de détection
-        self.recording_thread.join()
-        # Désactiver le bouton d'arrêt et activer le bouton de démarrage
-        self.stop_button.config(state=tk.DISABLED)
-        self.start_button.config(state=tk.NORMAL)
+        # Désactiver le bouton de démarrage
+        self.children["!button"].config(state=tk.DISABLED)
 
-        # Enregistrer le texte reconnu dans un fichier
-        filename = "enregistrement.wav"
-        with open(filename, "wb") as f:
-            f.write(self.audio.get_wav_data())
+        # Activer le bouton d'arrêt
+        self.children["!button2"].config(state=tk.NORMAL)
 
-        # Utiliser la reconnaissance vocale pour compter le nombre d'occurrences de chaque mot
-        result = {}
-        with sr.AudioFile(filename) as source:
-            audio_data = self.r.record(source)
-            text = self.r.recognize_google(audio_data, language="fr-FR")
-            for mot in self.mots:
-                occurrences = text.count(mot)
-                if occurrences > 0:
-                    result[mot] = occurrences
+        # Démarrer un thread pour l'enregistrement
+        self.thread = threading.Thread(target=self.recorder)
+        self.thread.start()
 
-        # Afficher le résultat dans l'étiquette de sortie
-        output_text = ""
-        for mot, occurrences in result.items():
-            output_text += f"{mot}: {occurrences}\n"
-        self.output_label.config(text=output_text)
+        # Démarrer un thread pour le comptage
+        self.thread_C = threading.Thread(target=self.update_counter)
+        self.thread_C.start()
 
-    def update_progressbar(self):
-        # Mettre à jour la barre de progression
-        elapsed_time = self.r.get_current_time() - self.progressbar_start_time
-        if elapsed_time < self.record_seconds * 1000:
-            self.progressbar["value"] = elapsed_time / (self.record_seconds * 10)
-            self.after(100, self.update_progressbar)
-        else:
-            self.progressbar["value"] = 100
+        # Démarrer un thread pour le comptage de tous les mots
+        self.thread_C_All_Words = threading.Thread(target=self.counter_json_file_all_words)
+        self.thread_C_All_Words.start()
 
-    def detect(self):
+    def stop_recording(self) -> None:
+        # Arrêter l'enregistrement
+        self.stop: bool = True
+
+        # Activer le bouton de démarrage
+        self.children["!button"].config(state=tk.NORMAL)
+
+        # Désactiver le bouton d'arrêt
+        self.children["!button2"].config(state=tk.DISABLED)
+
+    def recorder(self) -> None:
+        # Enregistrer le texte reconnu
         with self.microphone as source:
             self.r.adjust_for_ambient_noise(source)
-            self.audio = self.r.listen(source, phrase_time_limit=self.record_seconds)
+            while not self.stop:
+                self.info_label.config(text="Enregistrement en cours...")
+                audio = self.r.listen(source)
+                try:
+                    text = self.r.recognize_google(audio, language="fr-FR")
+                    self.textbox.insert(tk.END, text + "\n")
+                    self.textbox.see(tk.END)
+                except sr.UnknownValueError:
+                    self.info_label.config(text="Le texte n'a pas pu être reconnu")
+                    pass
+                except sr.RequestError as e:
+                    self.info_label.config(text="Erreur de connexion : " + str(e))
+                    MessageBox(str(e)).mainloop()
+                    break
+                else:
+                    self.info_label.config(text="Enregistrement terminé")
 
-        try:
-            text = self.r.recognize_google(self.audio, language="fr-FR")
-            self.textbox.insert(tk.END, text)
-        except:
-            pass
+        self.info_label.config(text="")
 
-        self.after(100, self.detect)
+    def count(self) -> None:
+        for mot in self.mots:
+            self.counter[mot] = 0
+
+        txt: str = self.textbox.get("1.0", tk.END)
+        txt = txt.lower()
+
+        for mot in self.mots:
+            self.counter[mot] = txt.count(mot)
+
+    def update_counter_affichage(self) -> None:
+        self.output_label.config(text=f"Nombre d'occurrences: {self.counter}")
+
+    def update_counter(self) -> None:
+        while not self.stop:
+            self.count()
+            self.update_counter_affichage()
+
+    def counter_json_file_all_words(self) -> None:
+        if not os.path.exists(self.Name_of_the_file_to_save_the_counter):
+            with open(self.Name_of_the_file_to_save_the_counter, "w") as f:
+                f.write("{}")
+        while not self.stop:
+            with open("counter.json", "w") as f:
+                json.dump(self.update_counter_all_words(), f, indent=4)
+
+    def update_counter_all_words(self) -> dict:
+        txt: str = self.textbox.get("1.0", tk.END)
+        txt = txt.lower()
+        list_txt: list = txt.split()
+        Total_Counter: dict = {}
+        Done_words: list = []
+        for mot in list_txt:
+            if mot not in Done_words:
+                Done_words.append(mot)
+                Total_Counter[mot] = txt.count(mot)
+        return Total_Counter
 
 
 if __name__ == "__main__":
